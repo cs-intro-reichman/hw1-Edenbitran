8c53187e9c4f5c4623eeb89407596df724c2adfe
Edenbitran <eden.bitran@post.runi.ac.il>
2024-11-11 21:50:31 +0200

    adding all files

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,48 @@
+import java.util.Random;
+
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		/* lim= limit of random integers
+		creating an arrey with random integers*/ 
+		int lim = Integer.parseInt(args[0]);
+		int[] nums = new int[3];
+		for (int i = 0; i < nums.length; i++) {
+			Random rnd = new Random();
+			int rndN = (int) (rnd.nextInt(1, lim));
+			nums[i] = rndN;
+			System.out.println("nums " + nums[i]);
+		}
+		int a, b, c, min, mid, max;
+		min = Math.min(nums[0], nums[1]);
+		a = Math.min(min, nums[2]);
+		//System.out.println("min= " + a);
+
+		max = Math.max(nums[0], nums[1]);
+		c = Math.max(max, nums[2]);
+		//System.out.println("max= " + c);
+
+		mid= Math.max(nums[0], nums[1]);
+		b = Math.min(mid, nums[2]);
+		System.out.println("mid- "+mid+"\nb- "+b);
+
+		if (Math.max(nums[0], nums[1]) == Math.min(mid, nums[2])) {
+			b = Math.min(mid, nums[2]);
+		}
+
+		if (mid != b) {
+			if ((mid == a || mid == c) && (b != a && b != c)) {
+				b = b;
+			}
+			if ((b == a || b == c) && (mid != a && mid != c)) {
+				b = mid;
+			}
+		}
+			
+
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			System.out.print("min=" + a + " mid=" + b+ " max=" + c);
 	}
+
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -9,9 +9,26 @@ public class TimeFormat {
 		// It concatenates the empty string "" with the leftmost hour-digit. 
 		// It then concatenates the resulting string with the rightmost hour-digit,
 		// and then uses parseInt to cast the resulting string as an int.
-		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+	//	//int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+	//	//int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+		// Replace this comment with the rest of your code
+		int h = Integer.parseInt(args[0]);
+		int m = Integer.parseInt(args[1]);
+		System.out.println(h + " : " + m);
+		if (h >= 00 && h <= 11 && m <= 59) {
+			System.out.println(h + " : " + m + " AM");
+		}
+		else if (h >= 12 && h <= 23 && m <= 59){
+			if (h == 12) {
+				System.out.println(h + " : " + m + " PM");
+				}
+			for (int i = 1; i < 12; i++) {
+				if (12 + i == h) {
+					System.out.println(i + " : " + m + " PM");
+				}
+				
+			}
+		}
 	}
 }
