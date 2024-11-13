afccff51540d6a34f65fba20da1aa8eb6ea367c8
Edenbitran <eden.bitran@post.runi.ac.il>
2024-11-13 17:13:22 +0200

    Fvc

--- FVCalc.java
+++ FVCalc.java
@@ -13,7 +13,9 @@ public class FVCalc {
 		double nr = (100 + rate) / 100;
 		//System.out.println("new rate "+nr);
 		//After 25 years, $25000 saved at 6.0% will yield $107296
+		//After 25 years, a $250000 saved at 6.0%25 will yield $1072967
+		//After 2 years, a $100 saved at 10.0%25 will yield $121
 		double futureValue = currentValue * Math.pow(nr, n);
-		System.out.println("After "+n+" years, $"+currentValue + " saved at " + rate + "% will yield $" + (int) futureValue);
+		System.out.println("After "+n+" years, a $"+currentValue + " saved at " + rate + "%25 will yield $" + (int) futureValue);
 	}// Computes the future value of a saving investment.
 }
