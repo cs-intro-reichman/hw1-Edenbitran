5c1550051a105176f065244dabc89dde7562909a
Edenbitran <eden.bitran@post.runi.ac.il>
2024-11-13 09:54:42 +0200

    Bill3

--- Bill3.java
+++ Bill3.java
@@ -5,9 +5,9 @@ public class Bill3 {
 		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
+		
 		double totalBill = Double.parseDouble(args[3]);
 		double bill4one = (totalBill / 3);
-		System.out.println("Dear " + name1 + ", " + name2 + ", " + name3 + "\n  pay: " + Math.ceil(bill4one)
-				+ " shekels each.");
+		System.out.println("Dear " + name1 + ", " + name2 + ", " + name3 + " pay: " + Math.ceil(bill4one)+ " shekels each.");
 	}
 }
